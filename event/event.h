921ca05fa579b3a870aa9c6c9341387868e88fb3
Colin Holzman <colin.holzman@protonmail.com>
2019-11-10 10:11:07 -0500

    event doc

--- event/event.h
+++ event/event.h
@@ -13,15 +13,25 @@
 
 #define CONTAINER_OF(ptr, type, field_name) ((type *)(((char *)ptr) - offsetof(type, field_name)))
 
+/*
+event state
+*/
 struct event {
     struct list handlers;
 };
 
+/*
+event handler state
+*/
 struct event_handler {
     struct list_element element;
     void (*fun)(struct event *evt, void *ctx);
 };
 
+/*
+initialize an event handler
+fun is a function to be called when the subscribed event is published
+*/
 static inline int event_handler_init(struct event_handler *handler, void (*fun)(struct event *, void *)) {
     if (!handler) return -1;
     list_element_init(&handler->element);
@@ -29,22 +39,35 @@ static inline int event_handler_init(struct event_handler *handler, void (*fun)(
     return 0;
 }
 
+/*
+initialize an event
+*/
 static inline int event_init(struct event *evt) {
     if (!evt) return -1;
     list_init(&evt->handlers);
     return 0;
 }
 
+/*
+attach an event handler to an event
+*/
 static inline int event_subscribe(struct event *evt, struct event_handler *handler) {
     if (!evt || !handler) return -1;
     return list_append(&evt->handlers, &handler->element);
 }
 
+/*
+detach an event handler from an event
+*/
 static inline int event_unsubscribe(struct event *evt, struct event_handler *handler) {
     if (!evt || !handler) return -1;
     return list_remove(&evt->handlers, &handler->element);
 }
 
+/*
+publish an event
+ctx will be passed to the handler functions of subscribed handlers
+*/
 static inline int event_publish(struct event *evt, void *ctx) {
     if (!evt) return -1;
     struct list_iterator it;
