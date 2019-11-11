e39da861cc78b4b7c53db6ff71ec0d8c3c441ca1
Colin Holzman <cholzman@hotmelt-tech.com>
2019-11-11 12:47:22 -0500

    add slist singly linked list

---
+++ list/slist.h
@@ -0,0 +1,138 @@
+
+#ifndef SLIST_H
+#define SLIST_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+/*members and functions marked SLIST_PRIVATE are not part of the list interface*/
+#define SLIST_PRIVATE
+
+struct slist_element {
+    SLIST_PRIVATE struct slist_element *next;
+};
+
+/*
+initialize a slist_element
+*/
+static inline int slist_element_init(struct slist_element *element) {
+    if (!element) return -1;
+    element->next = NULL;
+    return 0;
+}
+
+/*
+causes 'element' to succeed 'after' in the list that 'after' is a member of
+returns 0 if the insert was successful
+*/
+static inline int slist_element_insert_after(struct slist_element *after, struct slist_element *element) {
+    if (!after || !element) return -1;
+    element->next = after->next;
+    after->next = element;
+    return 0;
+}
+
+/*
+removes the element following after
+returns 0 if an element was removed
+*/
+static inline int slist_element_remove_after(struct slist_element *after) {
+    if (!after) return -1;
+    if (!after->next) return 0;
+    after->next = after->next->next;
+    return 0;
+}
+
+struct slist {
+    SLIST_PRIVATE struct slist_element head;
+};
+
+/*
+initialize a list
+returns 0 if successful
+*/
+static inline int slist_init(struct slist *list) {
+    if (!list) return -1;
+    return slist_element_init(&list->head);
+}
+
+/*
+checks if a list is empty
+return 0 if the check was successful
+is_empty is used to store the result
+*/
+static inline int slist_is_empty(struct slist *list, bool *is_empty) {
+    if (!list || !is_empty) return -1;
+    *is_empty = list->head.next == NULL;
+    return 0;
+}
+
+/*
+prepends element to list
+return 0 if successful
+if element is already part of a list then the result is undefined
+*/
+static inline int slist_prepend(struct slist *list, struct slist_element *element) {
+    if (!list) return -1;
+    return slist_element_insert_after(&list->head, element);
+}
+
+/*
+appends element to list
+return 0 if successful
+if element is already part of a list then the result is undefined
+*/
+static inline int slist_append(struct slist *list, struct slist_element *element) {
+    if (!list || !element) return -1;
+    struct slist_element *last = &list->head;
+    while (last->next) {
+        last = last->next;
+    }
+    return slist_element_insert_after(last, element);
+}
+
+/*
+removes element from list
+return 0 if successful
+if element is a member of a list other than the first argument then the effect is undefined
+*/
+static inline int slist_remove(struct slist *list, struct slist_element *element) {
+    if (!list || !element) return -1;
+    struct slist_element *last = &list->head;
+    while (last->next) {
+        if (last->next == element)
+            break;
+        last = last->next;
+    }
+    if (!last->next) return -1;
+    return slist_element_remove_after(last);
+}
+
+struct slist_iterator {
+    SLIST_PRIVATE struct slist_element *current;
+};
+
+/*
+initialize a list iterator
+return 0 if successful
+*/
+static inline int slist_iterator_init(struct slist_iterator *it, struct slist *list) {
+    if (!it || !list) return -1;
+    it->current = &list->head;
+    return 0;
+}
+
+/*
+gets the next element from an iterator
+return 0 if successful
+element is used to store the next element
+*/
+static inline int slist_iterator_next(struct slist_iterator *it, struct slist_element **element) {
+    if (!it || !element) return -1;
+    if (!it->current->next) return -1;
+    it->current = it->current->next;
+    *element = it->current;
+    return 0;
+}
+
+#endif //SLIST_H
---
+++ list/test/test_slist.c
@@ -0,0 +1,82 @@
+
+#include <stdio.h>
+#include <assert.h>
+#include "../slist.h"
+
+#define CONTAINER_OF(ptr, type, field_name) ((type *)(((char *)ptr) - offsetof(type, field_name)))
+
+struct my_list_element {
+    struct slist_element element;
+    int value;
+};
+
+struct slist my_list;
+
+struct slist my_list2;
+
+int main(void) {
+    //initialize
+    assert(slist_init(&my_list) == 0);
+    slist_init(&my_list2);
+    
+    //check empty
+    bool is_empty;
+    assert(slist_is_empty(&my_list, &is_empty) == 0);
+    assert(is_empty);
+    
+    //create element
+    struct my_list_element e0;
+    assert(slist_element_init(&e0.element) == 0);
+    e0.value = 0;
+    
+    //prepend to my_list
+    assert(slist_prepend(&my_list, &e0.element) == 0);
+    assert(slist_is_empty(&my_list, &is_empty) == 0);
+    assert(!is_empty);
+    
+    //another element
+    struct my_list_element e1;
+    slist_element_init(&e1.element);
+    e1.value = 1;
+    assert(slist_append(&my_list, &e1.element) == 0);
+    
+    //iterator
+    struct slist_iterator my_iterator;
+    assert(slist_iterator_init(&my_iterator, &my_list) == 0);
+    
+    //get first element
+    struct slist_element *current;
+    assert(slist_iterator_next(&my_iterator, &current) == 0);
+    struct my_list_element *element = CONTAINER_OF(current, struct my_list_element, element);
+    assert(element->value == 0);
+    
+    //get second element
+    assert(slist_iterator_next(&my_iterator, &current) == 0);
+    element = CONTAINER_OF(current, struct my_list_element, element);
+    assert(element->value == 1);
+    
+    //no more elements
+    assert(slist_iterator_next(&my_iterator, &current) != 0);
+    
+    //remove element (end element)
+    assert(slist_remove(&my_list, &e1.element) == 0);
+
+    //recheck list
+    assert(slist_iterator_init(&my_iterator, &my_list) == 0);
+    assert(slist_iterator_next(&my_iterator, &current) == 0);
+    element = CONTAINER_OF(current, struct my_list_element, element);
+    assert(element->value == 0);
+    
+    //remove another element (first element)
+    assert(slist_remove(&my_list, &e0.element) == 0);
+    
+    //recheck list
+    assert(slist_iterator_init(&my_iterator, &my_list) == 0);
+    assert(slist_iterator_next(&my_iterator, &current) != 0);
+    
+    //try to remove again
+    assert(slist_remove(&my_list, &e0.element) != 0);
+    
+    printf("tests passed\r\n");
+    return 0;
+}
