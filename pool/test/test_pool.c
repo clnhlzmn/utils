10a3c04d1b4a9f28a17f4f55df58734aab60576a
Colin Holzman <cholzman@hotmelt-tech.com>
2019-11-12 09:22:26 -0500

    pool doc

--- pool/test/test_pool.c
+++ pool/test/test_pool.c
@@ -9,16 +9,26 @@ POOL(test, long, 10);
 struct pool_test my_pool;
 
 int main(void) {
-    assert(pool_test_init(&my_pool) == 0);;
+    //init instance
+    assert(pool_test_init(&my_pool) == 0);
+    //a place to store my allocations
     long *ints[10] = {0};
+    //do a lot of allocs and frees
     for (int _ = 0; _ < 100000; ++_) {
+        //pick an index
         int i = rand() % 10;
+        //if it's allocated
         if (ints[i]) {
+            //check the value
             assert(*ints[i] == i);
+            //free it
             pool_test_free(&my_pool, ints[i]);
+            //set to NULL so we know
             ints[i] = NULL;
         } else {
+            //else allocate
             ints[i] = pool_test_alloc(&my_pool);
+            //if successful set value
             if (ints[i])
                 *ints[i] = i;
         }
