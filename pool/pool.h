a665ec4dca118a295c1986e321d4cdd8c7fff2f2
Colin Holzman <cholzman@hotmelt-tech.com>
2019-11-12 14:15:57 -0500

    change struct pool_*_private to union pool_*_private. don't need free list pointer and user data at the same time

--- pool/pool.h
+++ pool/pool.h
@@ -8,12 +8,12 @@
 
 #define POOL(name, type, size)                                                  \
 struct pool_##name {                                                            \
-    struct pool_##name##_private {                                              \
+    union pool_##name##_private {                                               \
         struct slist_element element;                                           \
         type data;                                                              \
     };                                                                          \
     struct slist free_list;                                                     \
-    struct pool_##name##_private items[size];                                   \
+    union pool_##name##_private items[size];                                    \
 };                                                                              \
 static inline int pool_##name##_init(struct pool_##name *self) {                \
     if (!self) return -1;                                                       \
@@ -28,14 +28,14 @@ static inline type *pool_##name##_alloc(struct pool_##name *self) {             
     struct slist_element *head;                                                 \
     if (slist_remove_head(&self->free_list, &head))                             \
         return NULL;                                                            \
-    struct pool_##name##_private *item = (struct pool_##name##_private *)       \
-        (((char *)head) - offsetof(struct pool_##name##_private, element));     \
+        union pool_##name##_private *item = (union pool_##name##_private *)     \
+            (((char *)head) - offsetof(union pool_##name##_private, element));  \
     return &item->data;                                                         \
 }                                                                               \
 static inline void pool_##name##_free(struct pool_##name *self, type *obj) {    \
     if (!self || !obj) return;                                                  \
-    struct pool_##name##_private *item = (struct pool_##name##_private *)       \
-        (((char *)obj) - offsetof(struct pool_##name##_private, data));         \
+    union pool_##name##_private *item = (union pool_##name##_private *)         \
+        (((char *)obj) - offsetof(union pool_##name##_private, data));          \
     slist_prepend(&self->free_list, &item->element);                            \
 }
 
