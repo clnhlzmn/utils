9b36caced245955d901bd9d7e620c5652493526d
Colin Holzman <cholzman@hotmelt-tech.com>
2019-11-12 09:11:35 -0500

    add pool test

--- pool/test/test_pool.c
+++ pool/test/test_pool.c
@@ -1,4 +1,6 @@
+
 #include "../pool.h"
+#include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
 
@@ -7,6 +9,19 @@ POOL(test, long, 10);
 struct pool_test my_pool;
 
 int main(void) {
-    assert(pool_test_init(&my_pool) == 0);
+    assert(pool_test_init(&my_pool) == 0);;
+    long *ints[10] = {0};
+    for (int _ = 0; _ < 100000; ++_) {
+        int i = rand() % 10;
+        if (ints[i]) {
+            assert(*ints[i] == i);
+            pool_test_free(&my_pool, ints[i]);
+            ints[i] = NULL;
+        } else {
+            ints[i] = pool_test_alloc(&my_pool);
+            if (ints[i])
+                *ints[i] = i;
+        }
+    }
     printf("tests passed\r\n");
 }
